c4ee998ef22a2a7d5c2b11b401a9ed3d84e829f3
Andrew Garrahan <andrew@computerlunch.com>
2017-02-01 16:29:32 -0500

    init upload of unity vr examples

---
+++ VRLunchExamples/Assets/AvaterController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AvaterController : MonoBehaviour {
+
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+
+		// Get this animator
+		anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetKey(KeyCode.Alpha1)){
+
+			anim.Play("talking_on_a_cell_phone");
+		}
+
+		if(Input.GetKey(KeyCode.Alpha2)){
+
+			anim.Play("walking");
+		}
+
+		if(Input.GetKey(KeyCode.Alpha3)){
+			anim.Play("bashful");
+		}
+
+		if(Input.GetKey(KeyCode.Alpha4)){
+
+			anim.Play("ymca_dance");
+		}
+	}
+}
---
+++ VRLunchExamples/Assets/Jump.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jump : MonoBehaviour {
+
+	Vector3 delta;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
