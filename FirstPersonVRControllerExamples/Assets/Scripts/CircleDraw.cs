9b65175d9b9e94f0fd8f4d4ea3e1a6399542c884
Andrew Garrahan <andrew@computerlunch.com>
2017-02-17 19:38:44 -0500

    FirstPersonMovement example

---
+++ FirstPersonVRControllerExamples/Assets/Scripts/CircleDraw.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections; 
+
+public class CircleDraw : MonoBehaviour {   
+	public float theta_scale = 0.01f;        //Set lower to add more points
+	int size; //Total number of points in circle
+
+	LineRenderer lineRenderer;
+
+	public float radius = 3f;
+	public float lineStartWidth = 1.0f;
+	public float lineEndWidth = 1.0f;
+	public float alpha = 1.0f; // Seems to be full
+
+	const float COMPLETE_CIRCLE_ARC = 0.64f;
+
+	public Color color;
+
+	int numSegments = 180;
+
+	void Awake () {       
+		   
+		lineRenderer = gameObject.AddComponent<LineRenderer>();
+		lineRenderer.material = new Material(Shader.Find("Unlit/Color"));
+		lineRenderer.material.color = color;
+	}
+
+	void BuildArcMesh(){
+
+		float sizeValue = ( Mathf.PI) / theta_scale; 
+		size = (int)sizeValue;
+		size++;
+
+		numSegments = (int)(size * alpha * COMPLETE_CIRCLE_ARC);
+
+		lineRenderer.SetWidth(lineStartWidth,lineEndWidth); //thickness of line
+		lineRenderer.SetVertexCount(numSegments); 
+		lineRenderer.useWorldSpace = false;
+	}
+
+	void FixedUpdate () {   
+
+		BuildArcMesh();
+
+		Vector3 pos;
+		float theta = 0f;
+		for(int i = 0; i < numSegments ; i++){          
+			theta += ( Mathf.PI * theta_scale);         
+			float x = radius * Mathf.Cos(theta);
+			float y = radius * Mathf.Sin(theta);          
+			x += gameObject.transform.position.x;
+			y += gameObject.transform.position.y;
+			pos = new Vector3(x, y, 0);
+			lineRenderer.SetPosition(i, pos);
+		}
+	}
+}
---
+++ FirstPersonVRControllerExamples/Assets/Scripts/EnemyZombie.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyZombie : MonoBehaviour {
+
+	Transform target; //the enemy's target
+	float moveSpeed = 125f; //move speed
+	float rotationSpeed = 2; //speed of turning
+	bool isAttack = true;
+	Transform myTransform; //current transform data of this enemy
+	CharacterController controller;
+
+	Animator anim;
+
+	void Awake()
+	{
+		myTransform = transform; //cache transform data for easy access/preformance
+		controller = GetComponent<CharacterController>();
+
+		anim = GetComponent<Animator>();
+	}
+
+	void Start()
+	{
+		target = GameObject.FindWithTag("Player").transform; //target the player
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if( Vector3.Distance(target.position ,myTransform.position) < 3.5){
+
+			if(isAttack == true){
+				anim.Play("atack01");
+				isAttack = false;
+			}
+
+
+		} else if( Vector3.Distance(target.position ,myTransform.position) < 10){
+
+			isAttack = true;
+			anim.Play("run02");
+
+		}else{
+
+			isAttack = false;
+			anim.Play("idle01");
+		}
+
+
+		if(isAttack){
+
+			// find the target direction:
+			Vector3 dir = target.position - myTransform.position;
+			dir.y = 0; // ignore height differences to avoid enemy tilting
+			//rotate to look at the player
+			myTransform.rotation = Quaternion.Slerp(myTransform.rotation, Quaternion.LookRotation(dir), rotationSpeed*Time.deltaTime);
+			//move towards the player:
+			//controller.SimpleMove(myTransform.forward * moveSpeed);
+
+		//	myTransform.position += myTransform.forward * moveSpeed * Time.deltaTime;
+			controller.SimpleMove( myTransform.forward * moveSpeed * Time.deltaTime);
+		}
+	}
+}
